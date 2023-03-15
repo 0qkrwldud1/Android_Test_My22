0e974b0e5e553f49dcf7212ba036803cc679cde5
0qkrwldud1 <0qkrwldud1@naver.com>
2023-03-15 16:59:23 +0900

    230315 저장..

--- test13/src/main/java/com/example/test13/LoginActivity.kt
+++ test13/src/main/java/com/example/test13/LoginActivity.kt
@@ -12,37 +12,37 @@ class LoginActivity : AppCompatActivity() {
         val binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val id = binding.id.text
-//        val pw = binding.id.text
-//
-//        binding.button1.setOnClickListener {
-//            val intent: Intent = Intent(this, MainActivity::class.java)
-//            intent.putExtra("id", "$id")
-//            intent.putExtra("pw", "$pw")
-//            startActivity(intent)
-//            // 메인에서 디테일으로 이동
-//        }
+        val id = binding.id.text
+        val pw = binding.id.text
+
+        binding.button1.setOnClickListener {
+            val intent: Intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("id", "$id")
+            intent.putExtra("pw", "$pw")
+            startActivity(intent)
+            // 메인에서 디테일으로 이동
+        }
     }
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        val binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val data1 = savedInstanceState.getString("id")
-        val data2 = savedInstanceState.getString("pw")
-
-        binding.id.hint="$data1"
-        binding.pw.hint = "$data2"
+//        val binding = ActivityLoginBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        val data1 = savedInstanceState.getString("id")
+//        val data2 = savedInstanceState.getString("pw")
+//
+//        binding.id.hint="$data1"
+//        binding.pw.hint = "$data2"
 
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        val binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        Log.d("kkang","onSaveInstanceState..........")
-
-        outState.putString("id", binding.id.text.toString())
-        outState.putString("pw", binding.pw.text.toString())
+//        val binding = ActivityLoginBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//        Log.d("kkang","onSaveInstanceState..........")
+//
+//        outState.putString("id", binding.id.text.toString())
+//        outState.putString("pw", binding.pw.text.toString())
     }
 }
